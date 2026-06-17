46aee3dcbd00dfa1cd2883e934a994ffc29681ca
ASHISHBISHT-gi <syntanalprime@gmail.com>
2026-06-17 10:25:04 +0530

    frontend finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import { useState , useEffect} from 'react';
-
+// import './App.css';
 function Header({score,bestscore}){
   return(
-    <div>
-      header 
-      {score}
-      {bestscore}
-      iron man is tony stark
+    <header>
+    <h2>Memory Cards</h2>
+    <div className="scoreboard"> 
+     <h3>Score: {score}</h3>
+     <h3>Bestscore: {bestscore}</h3>
     </div>
+    </header>
   )
 }
 async function imageurl(name){
    let data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`); 
    let result = await data.json();
    let  imgg= await result.sprites['front_default'];
+   console.log("imgg is ",imgg);
    return imgg;
    
 }
 
 function Image({name}){
-  const [url,setUrl] = useState(name);  
+  const [url,setUrl] = useState(" ");  
   useEffect(()=>{
     async function loadimage(){
       setUrl(await imageurl(name));
@@ -27,21 +29,21 @@ function Image({name}){
     loadimage();
   },[]);
     return(
-    <div>
+    <div className="pokecards">
       {console.log("first")}
-      <p>i am iron man</p>
-      <img src={url} alt="mewtwo" />
+      <img src={url} alt={name} />
       {console.log("sec")}
     </div>
   )
 }
 
-const pokemons=['charizard','mewtwo','ditto','mew','charmelon','bulbasaur','arceus','palkia','dialga','giratina','riolu','lucario','pickachu','raichu','zekrom','regiice','kyurem'];
 
-function Mid(){
+function Mid({pokemons}){
   return(
-    <div>
-       <Image name="mewtwo"></Image>
+    <div className="mid">
+      {pokemons.map((pokemon,index)=>(
+        <Image name={pokemon} key={pokemon} />
+      ))}
     </div>
   )
 }
@@ -50,10 +52,12 @@ export default function App(){
   // current score and best score
   const [score , changeScore] = useState(0);
   const [bestscore , changebestScore] = useState(0);
+  const [pokemons , setPokemons]=useState(['charizard','mewtwo','ditto','mew','charmander','bulbasaur','arceus','palkia','dialga','salamence','riolu','lucario','pikachu','raichu','zekrom','darkrai','kyurem','alakazam']);
+  
   return(
       <main>
       <Header score={score} bestscore={bestscore} />
-      <Mid />
+      <Mid pokemons={pokemons} />
       </main>
 )
 }
